54b06066003f844ef5e3ce407be1adc1c6ee4a12
Anutestleaf <anuspore14@gmail.com>
2025-08-17 15:10:15 +0800

    Add files via upload
    Day 1 Practices and Home Assignment

---
+++ EmployeeDetails.java
@@ -0,0 +1,32 @@
+package week1.day1;
+
+public class EmployeeDetails {
+	
+	public static void main(String[] args) {
+		
+		String name="John Doe";	
+		int age=30;		
+		double salary=55000.75;		
+		char gender='M';		
+		boolean acceptTerms=true;		
+		float rating=4.5f;		
+		String mobileNumber="9876543210";	
+		int pinCode=56001;
+		int luckyNumber=7;
+		
+		System.out.println(
+						"Employee Name is "+name+ "\n" + 
+						"Employee Age is "+age+ "\n" +
+						"Employee Salary is "+salary+ "\n" +
+						"Employee gender is "+gender+ "\n" +
+						"Employee has Accepted Terms="+acceptTerms+ "\n" +
+						"Employee Company Rating is "+rating+ "\n" +
+						"Employee Mobile Number="+mobileNumber+ "\n" +
+						"Employee Pincode="+pinCode+ "\n" +
+						"Employee Lucky Number is "+luckyNumber+ "\n"
+				);
+		
+	
+	}
+
+}
---
+++ Firefox.java
@@ -0,0 +1,32 @@
+package week1.day1;
+
+public class Firefox {
+	
+	public static void main(String[] args) {
+		
+		double browserVersion=100.2;
+		
+				String browserName="firefox";
+				
+				boolean isVisible=true;
+				
+				int releaseYear=1998;
+				
+				char browserLogo='f';
+				
+			System.out.println("The Value of browserVersion ="+browserVersion+"\n" +
+			
+					"The Value of browserName="+browserName+ "\n" + 
+					
+					"The Value of isVisible="+isVisible+ "\n" + 
+					
+					"The Value of releaseYear="+releaseYear +"\n" +
+					
+					"The Value of browserLogo="+browserLogo );
+		
+	}
+
+}
+
+
+
